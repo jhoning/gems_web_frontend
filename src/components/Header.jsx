24b800cf4123886bc6fefab3c7697fa57033586a
Rebeca Paredes <rebecavparedes@gmail.com>
2021-03-10 21:08:40 -0400

    login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next';
 import i18next from 'i18next';
-import '../css/btn.css'
+import '../css/btn.css';
+import es from '../css/espana.svg';
+import en from '../css/estados-unidos.svg';
 const Header = () => {
   const [t] = useTranslation("global")
 
@@ -11,40 +13,41 @@ const Header = () => {
   return (
     <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 shadow-sm fondo">
       <h5 className="my-0 mr-md-auto text-light ml-md-4">T-board</h5>
-      <nav className="my-2 my-md-0 mr-md-3">
+      
+      <nav className="my-2 my-md-0 mr-md-1">
         <a className="p-2 text-light" href="">{t("Header.plans_pricing")}</a>
         <a className="p-2 text-light" href="">{t("Header.help")}</a>
       </nav>
 
-{/*       <ul class="navbar-nav idioma mr-md-1">
+    <ul class="navbar-nav idioma mr-md-1">
       <li class="nav-item dropdown language-dropdown">
         <a class="nav-link dropdown-toggle px-2 d-flex align-items-center leng text-light" id="LanguageDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
           <div class="d-inline-flex mr-0 mr-md-3">
             <div class="flag-icon-holder"><i class="flag-icon flag-icon-us"></i>
             </div>
           </div>
-          <img class="ban" src={en}>
+            <img class="ban" src={en} />
             <span class="profile-text font-weight-medium d-none d-md-block">English</span>
         </a>
         <div class="dropdown-menu dropdown-menu-left navbar-dropdown py-2" aria-labelledby="LanguageDropdown">
-          <a class="dropdown-item" id="id_es">
+          <a class="dropdown-item" id="id_es" onClick={() => handleClick('es')}>
             <div class="flag-icon-holder">
-              <img class="ban" src={es}>
+                <img class="ban" src={es} />
                 <span class="text-light">Spanish</span>
             </div>
           </a>
-          <a class="dropdown-item" id="id_en">
+          <a class="dropdown-item" id="id_es" onClick={() => handleClick('en')}>
             <div class="flag-icon-holder">
-              <img class="ban" src={en}>
+                <img class="ban" src={en} />
                 <span class="text-light">English</span>
             </div>
           </a>
         </div>
       </li>
-      </ul> */}
-      <button className="btn btn-primary mx-1" id="id_es" onClick={() => handleClick('es')}
+      </ul>  
+      {/* <button className="btn btn-primary mx-1" id="id_es" onClick={() => handleClick('es')}
       >es</button>
-      <button className="btn btn-primary" id="id_es" onClick={() => handleClick('en')}>en</button>
+      <button className="btn btn-primary" id="id_es" onClick={() => handleClick('en')}>en</button> */}
       <a className="btn btn-outline-primary anchoButton mx-1" href="/register">{t("Header.sign_up")}</a>
       <a className="btn btn-outline-primary anchoButton" href="/">{t("Header.sign_in")}</a>
     </div>
