8bc209539a0834763f687bb9007ea8e785a018eb
Rebeca Paredes <rebecavparedes@gmail.com>
2021-03-10 22:33:12 -0400

    login y register

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next';
 const Footer = () => {
   const [t] = useTranslation("global")
   return (
-    <div className="container-fluid p-0">
+    <div className="container-fluid p-0 foot">
       <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3">
         <h5 className="my-0 mr-md-auto"></h5>
         <nav className="my-2 my-md-0 mr-md-3">
