a0c7b2b4e119bb5a590796a7f931b3de09ef925d
Rebeca Paredes <rebecavparedes@gmail.com>
2021-03-19 12:18:13 -0400

    mejoras en calculate

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   const [t] = useTranslation("global")
   return (
     <div className="container-fluid p-0 foot">
-      <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3">
+      <div className="d-flex flex-column flex-md-row align-items-center ">
         
         <h5 className="my-0 mr-md-auto"></h5>
         <nav className="my-2 my-md-0 mr-md-3">
