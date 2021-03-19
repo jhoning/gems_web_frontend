7c5b3053a84ed3a69e8f02ea19f504d20927c087
Rebeca Paredes <rebecavparedes@gmail.com>
2021-03-19 16:59:18 -0400

    modificaciones solicitadas

--- src/components/HeaderT.jsx
+++ src/components/HeaderT.jsx
@@ -67,7 +67,7 @@ const Header = () => {
       <a className="p-2 text-light anchoHelp mr-md-3" href="">{t("Header.help")}</a>
 
       <a className="btn btn-outline-light text-light mx-1 mr-md-3 line" href="/user_settings">{t("userS.userS")}</a>
-      <a className="btn btn-outline-light text-light mx-1 mr-md-3 line" href="/calculate">Calculate</a>
+      <a className="btn btn-outline-light text-light mx-1 mr-md-3 line" href="/calculate">{t("Header.calculate")}</a>
       <a className="btn btn-outline-light mr-md-4 check text-light"  onClick={()=>deslogear()}>{ t("HeaderT.log_out")}</a>
         
     </div>
