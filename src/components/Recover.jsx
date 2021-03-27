4a66c17efbdde44a1e939cd4871b0a5bfeb22914
llealg <lleal.js@gmail.com>
2021-03-27 16:04:56 -0400

    solucionada el error del correo y recuperacion

--- src/components/Recover.jsx
+++ src/components/Recover.jsx
@@ -8,7 +8,7 @@ const Recover = () => {
   const [email,setEmail] = useState()
   const changePassword = async() => {
     await axios.put('http://localhost:4000/auth/forgot-password',
-      {email:email}
+      email
   
   ).then((res)=>console.log("bueno")).catch(err => console.log("error"))
   }
@@ -21,14 +21,14 @@ const Recover = () => {
               <div id="recovern-box" className="col-md-12">
                 <h3 className="text-center mb-4">Ingresa tu correo</h3>
                 <p class="mensaje">Por favor ingrese su correo electrónico para recuperar su contraseña</p>
-               
+                <form >
                   <div class="form-group clave">
                     <input type="text" name="email" id="email" placeholder="ingrese correo" className="form-control"  onChange={ e => setEmail({email: e.target.value}) }/>
                   </div>
                   <div className="form-group text-center mt-3">
                     <button  className="boton btn btn-primary" onClick={()=>{changePassword()}}> enviar</button>
                   </div>
-             
+                </form>
               </div>
             </div>
           </div> 
