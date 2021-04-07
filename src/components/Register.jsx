adab1aa6c13c557cec8c25deb1f42358b65af773
Rebeca Paredes <rebecavparedes@gmail.com>
2021-04-06 20:25:45 -0400

    icon clave registro

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -59,12 +59,13 @@ const Register = () => {
 
           if ( password1.type === "text" ) {
               password1.type = "password"
-           
-              showPassword.classList.remove('fa-eye-slash');
+              showPassword.classList.remove('fa-eye');  
+              showPassword.classList.toggle('fa-eye-slash');
           } else {
               password1.type = "text"
        
-              showPassword.classList.toggle("fa-eye-slash");
+              showPassword.classList.toggle("fa-eye");
+              showPassword.classList.remove('fa-eye-slash');
           }
 
       })
