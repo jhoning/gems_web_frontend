544cf44f486d339ad4e27fa666ae9650e6a2415b
Rebeca Paredes <rebecavparedes@gmail.com>
2021-04-15 17:40:46 -0400

    cambios front

--- src/components/NavTree.jsx
+++ src/components/NavTree.jsx
@@ -5,6 +5,9 @@ import { useTranslation } from 'react-i18next';
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import BoardMenu from './BoardMenu'
+import circuit from '../img/icon1.svg';
+import tab from '../img/iconplus.svg';
+import plus from '../img/plus.svg';
 import '../css/tree-nav.css'
 import '../../node_modules/react-simple-tree-menu/dist/main.css';
 import { Tree } from 'primereact/tree';
@@ -18,7 +21,7 @@ const authAxios = axios.create({
 })  
 
 const NavTree = ({idCircuits})=> {
-
+  const [t] = useTranslation("global")
   const [arrBoards,SetArrBoards] = useState()
   const [arrBoardsInfo,SetArrBoardsInfo] = useState([])
   const [arrBoardsPrincipales,SetArrBoardsPrincipales] = useState([])
@@ -131,20 +134,42 @@ const NavTree = ({idCircuits})=> {
     
     if (node.label) {
       return (
-        <div  style={{height: '70px',padding: '0px',margin:'0px'}}>
+        <div  style={{height: '70px',padding: '20px 0 0 0px',margin:'0px 6px 0 0'}}>
           <span onClick={()=>console.log('ola')}>{node.label}</span>
-          <button className="btn btn-primary btn-sm ml-4 " onClick={()=>{
-      /*     console.log(circuits)
+          {/* <button className="btn btn-primary btn-sm ml-4 " onClick={()=>{
+          console.log(circuits)
           obtenerBoard(node.key)
         
-          crearCircuit(node.key) */
-      }}>C</button>  
-          <button className="btn btn-primary btn-sm ml-4 " onClick={()=>{
+          crearCircuit(node.key) 
+      }}>C</button>  */} 
+          {/* <button className="btn btn-primary btn-sm ml-4 " onClick={()=>{
               registrarBoard(node.key,node.label)
               setBandera(bandera? false:true)
-             /*  setBoards(recorre(boards,node.key))
-              crearCircuit(node.key) */
-          }}>+</button>  
+              setBoards(recorre(boards,node.key))
+              crearCircuit(node.key) 
+          }}>+</button>   */}
+           <ul class="navbar-nav idioma mr-md-1 fr">
+        <li class="nav-item dropdown language-dropdown">
+            <a class="" id="LanguageDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
+              <img class="ban" src={plus} />
+            </a> 
+
+          <div class="dropdown-menu dropdown-menu-left navbar-dropdown py-2" aria-labelledby="LanguageDropdown">
+            <a class="dropdown-item" id="id_es" >
+              <div class="flag-icon-holder">
+                <img class="ban" src={circuit} />
+                <span class="text-dark">{t("MenuTree.addAppliance")}</span>
+              </div>
+            </a>
+            <a class="dropdown-item" id="id_en" >
+              <div class="flag-icon-holder">
+                <img class="ban" src={tab} />
+                <span class="text-dark">{t("MenuTree.addBoard")}</span>
+              </div>
+            </a>
+          </div>
+        </li>
+      </ul>
         </div>
           
       )
@@ -155,7 +180,7 @@ const NavTree = ({idCircuits})=> {
       <button className='btn btn-primary' onClick={()=>{
         registrarBoard1()
 
-      }}>Agregar Board</button>
+      }}>{t("MenuTree.addBoard")}</button>
       <Tree value={arrBoardsPrincipales} nodeTemplate={nodeTemplate} className="mx-0" />
     </>
   )
