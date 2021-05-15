0025e4aa7f1be0ff0f0eb34b0663a77fae46ef12
Rebeca Paredes <rebecavparedes@gmail.com>
2021-05-14 21:06:03 -0400

    fix

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-
-import { LoginForm } from '../containers/LoginForm';
-import Register from '../components/Register';
-import Home_tboard from '../containers/Home_tboard';
-import UserSettings from '../components/UserSettings';
-import Calculate1 from '../containers/Calculate1';
-import Recover from '../containers/Recover';
-import ChangePassword from '../containers/ChangePassword';
-import MessageEmail from '../components/MessageEmail';
-import Code from '../containers/Code';
-
-export default function Routes() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={LoginForm}/>
-        <Route exact path="/register" component={Register}/>
-        <Route exact path="/recover" component={Recover}/>
-        <Route exact path="/code" component={Code}/>
-        <Route path="/message/:token" component={MessageEmail}/>
-        <Route path="/change/:token" children={ChangePassword}/>
-        <Route exact path="/t_board" component={Home_tboard}/>
-        <Route exact path="/user_settings" component={UserSettings}/>
-        <Route path="/calculate/:id1" component={Calculate1} />
-     </Switch>
-    </BrowserRouter>
-  );  
-=======
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
@@ -58,5 +27,4 @@ export default function Routes() {
      </Switch>
     </BrowserRouter>
   );  
->>>>>>> c2e584467b7cb0e5ea79244f153bb3af329b8bd9
 }
