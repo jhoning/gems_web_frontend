fbce001ccad04c52c17b930be4cf65ce6336beef
llealg <lleal.js@gmail.com>
2021-05-17 08:17:31 -0400

    logica del navtree compleatadagit add .!

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useEffect,useState,Component} from 'react'
+import React, { useEffect, useState, Component } from 'react'
 import { Tree } from 'primereact/tree';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
@@ -8,605 +8,127 @@ const App = () => {
   /*tableros guarda el arreglo de todos los boards asocido a un proyecto*/
   const [tableros, setTableros] = useState([
     {
-      id: "27",
-      name: "ht",
+      id: "1",
+      name: "tablero",
       project: {
         id: "19",
         name: "kjhk",
       },
       circuits: [
-        {
-          id: "1",
-          name: "Circuit",
-        },
-        {
-          id: "3",
-          name: "Circuit",
-        },
-        {
-          id: "4",
-          name: "Circuit",
-        },
-        {
-          id: "5",
-          name: "Circuit",
-        },
-        {
-          id: "6",
-          name: "Circuit",
-        },
-        {
-          id: "7",
-          name: "Circuit",
-        },
-        {
-          id: "9",
-          name: "Circuit",
-        },
-        {
-          id: "10",
-          name: "Circuit",
-        },
-        {
-          id: "11",
-          name: "Circuit",
-        },
-        {
-          id: "12",
-          name: "Circuit",
-        },
-        {
-          id: "13",
-          name: "Circuit",
-        },
-        {
-          id: "14",
-          name: "Circuit",
-        },
-        {
-          id: "15",
-          name: "Circuit",
-        },
-        {
-          id: "16",
-          name: "Circuit",
-        },
-        {
-          id: "17",
-          name: "Circuit",
-        },
-        {
-          id: "18",
-          name: "Circuit",
-        },
-        {
-          id: "19",
-          name: "Circuit",
-        },
-        {
-          id: "20",
-          name: "Circuit",
-        },
-        {
-          id: "21",
-          name: "Circuit",
-        },
-        {
-          id: "22",
-          name: "Circuit",
-        },
-        {
-          id: "23",
-          name: "Circuit",
-        },
-        {
-          id: "27",
-          name: "Circuit",
-        },
-        {
-          id: "28",
-          name: "Circuit",
-        },
-        {
-          id: "68",
-          name: "Circuit",
-        },
-        {
-          id: "69",
-          name: "Circuit",
-        },
-        {
-          id: "70",
-          name: "Circuit",
-        },
-        {
-          id: "71",
-          name: "Circuit",
-        },
-        {
-          id: "72",
-          name: "Circuit",
-        },
-        {
-          id: "73",
-          name: "Circuit",
-        },
-        {
-          id: "74",
-          name: "Circuit",
-        },
-        {
-          id: "75",
-          name: "Circuit",
-        },
-        {
-          id: "76",
-          name: "Circuit",
-        },
-        {
-          id: "77",
-          name: "Circuit",
-        },
+       
       ],
       board_padre: null,
-      board_hijos: [
-         {
-                "id": "36",
-                "name": "nuevo board"
-            },
-            {
-                "id": "39",
-                "name": "nuevo board"
-            },
-            {
-                "id": "44",
-                "name": "nuevo board"
-            }
+      
+    },
+    {
+      id: "2",
+      name: "tablero",
+      project: {
+        id: "19",
+        name: "kjhk",
+      },
+      circuits: [
+       
       ],
+      board_padre: {
+        id:1
+      },
+      
+      
     },
     {
-        "id": "28",
-        "name": "ht",
-        "project": {
-            "id": "19",
-            "name": "kjhk"
-        },
-        "circuits": [
-            {
-                "id": "2",
-                "name": "Circuit"
-            },
-            {
-                "id": "24",
-                "name": "Circuit"
-            },
-            {
-                "id": "66",
-                "name": "Circuit"
-            },
-            {
-                "id": "67",
-                "name": "Circuit"
-            }
-        ],
-        "board_padre": null,
-        "board_hijos": [
-            {
-                "id": "110",
-                "name": "Nuev"
-            },
-            {
-                "id": "111",
-                "name": "Nuev"
-            }
-        ]
+      id: "3",
+      name: "tablero",
+      project: {
+        id: "19",
+        name: "kjhk",
+      },
+      circuits: [
+       
+      ],
+      board_padre: {
+        id:1
+      },
+      
     },
     {
-        "id": "29",
-        "name": "asd",
-        "project": {
-            "id": "19",
-            "name": "kjhk"
-        },
-        "circuits": [
-            {
-                "id": "8",
-                "name": "Circuit"
-            },
-            {
-                "id": "25",
-                "name": "Circuit"
-            },
-            {
-                "id": "26",
-                "name": "Circuit"
-            }
-        ],
-        "board_padre": null,
-        "board_hijos": [
-            {
-                "id": "115",
-                "name": "Nuev"
-            },
-            {
-                "id": "116",
-                "name": "Nuev"
-            }
-        ]
+      id: "4",
+      name: "tablero",
+      project: {
+        id: "19",
+        name: "kjhk",
+      },
+      circuits: [
+       
+      ],
+      board_padre: {
+        id:3
+      },
+      
     },
     {
-        "id": "30",
-        "name": "sdf",
-        "project": {
-            "id": "19",
-            "name": "kjhk"
-        },
-        "circuits": [],
-        "board_padre": null,
-        "board_hijos": []
+      id: "5",
+      name: "tablero",
+      project: {
+        id: "19",
+        name: "kjhk",
+      },
+      circuits: [
+       
+      ],
+      board_padre: {
+        id:3
+      },
+      
     },
     {
-        "id": "36",
-        "name": "nuevo board",
-        "project": {
-            "id": "19",
-            "name": "kjhk"
-        },
-        "circuits": [],
-        "board_padre": {
-            "id": "27",
-            "name": "ht3"
-        },
-        "board_hijos": [
-            {
-                "id": "109",
-                "name": "Nuev"
-            },
-            {
-                "id": "112",
-                "name": "Nuev"
-            },
-            {
-                "id": "113",
-                "name": "Nuev"
-            },
-            {
-                "id": "114",
-                "name": "Nuev"
-            }
-        ]
+      id: "6",
+      name: "tablero",
+      project: {
+        id: "19",
+        name: "kjhk",
+      },
+      circuits: [
+       
+      ],
+      board_padre: {
+        id:5
+      },
+      
+      
     },
     {
-        "id": "39",
-        "name": "nuevo board",
-        "project": {
-            "id": "19",
-            "name": "kjhk"
-        },
-        "circuits": [],
-        "board_padre": {
-            "id": "27",
-            "name": "ht5"
-        },
-        "board_hijos": []
-    },
-    {
-        "id": "44",
-        "name": "nuevo board",
-        "project": {
-            "id": "19",
-            "name": "kjhk"
-        },
-        "circuits": [],
-        "board_padre": {
-            "id": "27",
-            "name": "ht"
-        },
-        "board_hijos": []
-    },
-    {
-        "id": "45",
-        "name": "Nuevo Board",
-        "project": {
-            "id": "19",
-            "name": "kjhk"
-        },
-        "circuits": [],
-        "board_padre": null,
-        "board_hijos": []
-    },
-    {
-        "id": "46",
-        "name": "Nuevo Board",
-        "project": {
-            "id": "19",
-            "name": "kjhk"
-        },
-        "circuits": [],
-        "board_padre": null,
-        "board_hijos": []
-    },
-    {
-        "id": "47",
-        "name": "Nuevo Board",
-        "project": {
-            "id": "19",
-            "name": "kjhk"
-        },
-        "circuits": [],
-        "board_padre": null,
-        "board_hijos": []
-    },
-    {
-        "id": "48",
-        "name": "Nuevo Board",
-        "project": {
-            "id": "19",
-            "name": "kjhk"
-        },
-        "circuits": [],
-        "board_padre": null,
-        "board_hijos": []
-    },
-    {
-        "id": "109",
-        "name": "Nuev",
-        "project": {
-            "id": "19",
-            "name": "kjhk"
-        },
-        "circuits": [],
-        "board_padre": {
-            "id": "36",
-            "name": "nuevo board"
-        },
-        "board_hijos": []
-    },
-    {
-        "id": "110",
-        "name": "Nuev",
-        "project": {
-            "id": "19",
-            "name": "kjhk"
-        },
-        "circuits": [],
-        "board_padre": {
-            "id": "28",
-            "name": "ht"
-        },
-        "board_hijos": [
-            {
-                "id": "117",
-                "name": "Nuev"
-            },
-            {
-                "id": "118",
-                "name": "Nuev"
-            },
-            {
-                "id": "119",
-                "name": "Nuev"
-            },
-            {
-                "id": "120",
-                "name": "Nuev"
-            },
-            {
-                "id": "121",
-                "name": "Nuev"
-            },
-            {
-                "id": "122",
-                "name": "Nuev"
-            },
-            {
-                "id": "123",
-                "name": "Nuev"
-            },
-            {
-                "id": "124",
-                "name": "Nuev"
-            }
-        ]
-    },
-    {
-        "id": "111",
-        "name": "Nuev",
-        "project": {
-            "id": "19",
-            "name": "kjhk"
-        },
-        "circuits": [],
-        "board_padre": {
-            "id": "28",
-            "name": "ht"
-        },
-        "board_hijos": []
-    },
-    {
-        "id": "112",
-        "name": "Nuev",
-        "project": {
-            "id": "19",
-            "name": "kjhk"
-        },
-        "circuits": [],
-        "board_padre": {
-            "id": "36",
-            "name": "nuevo board"
-        },
-        "board_hijos": []
-    },
-    {
-        "id": "113",
-        "name": "Nuev",
-        "project": {
-            "id": "19",
-            "name": "kjhk"
-        },
-        "circuits": [],
-        "board_padre": {
-            "id": "36",
-            "name": "nuevo board"
-        },
-        "board_hijos": []
-    },
-    {
-        "id": "114",
-        "name": "Nuev",
-        "project": {
-            "id": "19",
-            "name": "kjhk"
-        },
-        "circuits": [],
-        "board_padre": {
-            "id": "36",
-            "name": "nuevo board"
-        },
-        "board_hijos": []
-    },
-    {
-        "id": "115",
-        "name": "Nuev",
-        "project": {
-            "id": "19",
-            "name": "kjhk"
-        },
-        "circuits": [],
-        "board_padre": {
-            "id": "29",
-            "name": "asd"
-        },
-        "board_hijos": []
-    },
-    {
-        "id": "116",
-        "name": "Nuev",
-        "project": {
-            "id": "19",
-            "name": "kjhk"
-        },
-        "circuits": [],
-        "board_padre": {
-            "id": "29",
-            "name": "asd"
-        },
-        "board_hijos": []
-    },
-    {
-        "id": "117",
-        "name": "Nuev",
-        "project": {
-            "id": "19",
-            "name": "kjhk"
-        },
-        "circuits": [],
-        "board_padre": {
-            "id": "110",
-            "name": "Nuev"
-        },
-        "board_hijos": []
-    },
-    {
-        "id": "118",
-        "name": "Nuev",
-        "project": {
-            "id": "19",
-            "name": "kjhk"
-        },
-        "circuits": [],
-        "board_padre": {
-            "id": "110",
-            "name": "Nuev"
-        },
-        "board_hijos": []
-    },
-    {
-        "id": "119",
-        "name": "Nuev",
-        "project": {
-            "id": "19",
-            "name": "kjhk"
-        },
-        "circuits": [],
-        "board_padre": {
-            "id": "110",
-            "name": "Nuev"
-        },
-        "board_hijos": []
-    },
-    {
-        "id": "120",
-        "name": "Nuev",
-        "project": {
-            "id": "19",
-            "name": "kjhk"
-        },
-        "circuits": [],
-        "board_padre": {
-            "id": "110",
-            "name": "Nuev"
-        },
-        "board_hijos": []
-    },
-    {
-        "id": "121",
-        "name": "Nuev",
-        "project": {
-            "id": "19",
-            "name": "kjhk"
-        },
-        "circuits": [],
-        "board_padre": {
-            "id": "110",
-            "name": "Nuev"
-        },
-        "board_hijos": []
-    },
-    {
-        "id": "122",
-        "name": "Nuev",
-        "project": {
-            "id": "19",
-            "name": "kjhk"
-        },
-        "circuits": [],
-        "board_padre": {
-            "id": "110",
-            "name": "Nuev"
-        },
-        "board_hijos": []
-    },
-    {
-        "id": "123",
-        "name": "Nuev",
-        "project": {
-            "id": "19",
-            "name": "kjhk"
-        },
-        "circuits": [],
-        "board_padre": {
-            "id": "110",
-            "name": "Nuev"
-        },
-        "board_hijos": []
-    },
-    {
-        "id": "124",
-        "name": "Nuev",
-        "project": {
-            "id": "19",
-            "name": "kjhk"
-        },
-        "circuits": [],
-        "board_padre": {
-            "id": "110",
-            "name": "Nuev"
-        },
-        "board_hijos": []
+      id: "7",
+      name: "tablero",
+      project: {
+        id: "19",
+        name: "kjhk",
+      },
+      circuits: [
+       
+      ],
+      board_padre: {
+        id:6
+      },
+    }, {
+      id: "8",
+      name: "tablero",
+      project: {
+        id: "19",
+        name: "kjhk",
+      },
+      circuits: [
+       
+      ],
+      board_padre: {
+        id:7
+      },
     }
+    
+  
+    
   ]);
-  const [arr,setArr] = useState()
+  const [arr, setArr] = useState()
   const generarNavTree = () => {
     /*estas variables aux se encargar de separar los tableros cuyos padres son null (tableros principales) 
     y los que si poseen un id */
@@ -625,24 +147,52 @@ const App = () => {
       }
     })
 
-
-
-   console.log('Tableros padre: ', aux_padre_null);
+    console.log('Tableros padre: ', aux_padre_null);
     aux_padre_id = tableros.filter(item => item.board_padre != null)
     /*tomando encuenta que su key viene siendo la id del tablero que usaremos 
     mas adelante para comparar con el id padre y hacer las inserciones comparado su key(id) 
     con su board_padre del otro elemnto*/
-   
+
     /* ahora debemos recorrer los tableros hijos para ir insertandolos 
     del navtree ya antes generada(solo con tablero null)*/
     console.log('Tableros hijos: ', aux_padre_id);
-    const insertarTablero = (
-     // aux_padre_null,
+    const start = (node,id,id_hijo) => {
+      console.log(id,id_hijo)
+      if(node.key == id){
+        node.children.push({
+          "key": id_hijo,
+          "label": "Nuevo hijo" + id_hijo,
+          "data": "Nuevo hijo",
+          "icon": "pi pi-fw pi-sliders-h",
+          "children": [],
+        })
+      }
+   /*    console.log('Node',node.key) */
+      if(node.children){
+        node.children.forEach(function(child){
+          start(child,id,id_hijo)
+        }
+          
+        );
+      }
+    }
+    for (let i = 0; i < aux_padre_id.length; i++) {
+      for (let j = 0; j < aux_padre_null.length; j++) {
+        console.log('help',aux_padre_id[i].id)
+        console.log('Keys ',aux_padre_id[i].id,aux_padre_id[i].board_padre.id) 
+        start(aux_padre_null[j],aux_padre_id[i].board_padre.id,aux_padre_id[i].id)
+      }
+   
+    }
+  
+  
+  /*   const insertarTablero = (
+      aux_padre_null,
       padre_id,
       id,
       name
     ) => {
-      let aux = aux_padre_null.findIndex((item) => item.key == padre_id);
+      let aux = aux_padre_null.children.findIndex((item) => item.key == padre_id);
       if (aux != -1) {
         aux_padre_null[aux].children.push({
           key: id,
@@ -651,25 +201,33 @@ const App = () => {
           icon: "pi pi-fw pi-sliders-h",
           children: [],
         });
+      }else {
+        if(aux_padre_null != undefined){
+          for (let i = 0; i < aux_padre_null.children.length; i++) {
+            insertarTablero(aux_padre_null[i])
+            
+          }
+        }
       }
     };
-   
-     for (let i = 0; i < aux_padre_id.length; i++) {
-     // insertarTablero(aux_padre_null[i].children, aux_padre_id[i].board_padre.id, aux_padre_id[i].id, aux_padre_id[i].name)
-        insertarTablero(aux_padre_id[i].board_padre.id, aux_padre_id[i].id, aux_padre_id[i].name)
-    }
-    
+
+    for (let i = 0; i < aux_padre_id.length; i++) {
+
+      insertarTablero(aux_padre_null[i], aux_padre_id[i].board_padre.id, aux_padre_id[i].id, aux_padre_id[i].name)
+
+    } */
+
     console.log(aux_padre_null);
     setArr(aux_padre_null)
     //Hasta este punto del
   }
   //useEffect es la encargada de llamar a las funciones una vez ingresaste a la pagina
-  useEffect(()=>{
+  useEffect(() => {
     generarNavTree()
-  },[])
+  }, [])
   return (
     <>
-       <Tree value={arr} /> 
+      <Tree value={arr} />
     </>
   )
 }
