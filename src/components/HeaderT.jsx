6c0f17766acd507d4b0cab56dc70176cf55cc2b5
Rebeca Paredes <rebecavparedes@gmail.com>
2021-06-04 21:42:13 -0400

    fix

--- src/components/HeaderT.jsx
+++ src/components/HeaderT.jsx
@@ -65,12 +65,12 @@ const Header = () => {
         </li>
       </ul>
       </nav>
-      <a className="text-dark anchoHelp help mr-2" href="">{t("Header.help")}</a>
+      <a className="text-dark anchoHelp help mr-1" href=""><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-help-circle"><circle cx="12" cy="12" r="10"></circle><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path><line x1="12" y1="17" x2="12.01" y2="17"></line></svg>{t("Header.help")}</a>
 
-      {URLactual == "http://localhost:3004/user_settings"? null: <a className="btn btn-outline-light text-dark mx-1 mr-md-2 line" href="/user_settings">{t("userS.userS")}</a>}
+      {URLactual == "http://localhost:3004/user_settings"? null: <a className="btn btn-outline-light text-dark mx-1 mr-md-2 line usuario" href="/user_settings"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-3"><path d="M12 20h9"></path><path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path></svg>{t("userS.userS")}</a>}
       
       {URLactual == "http://localhost:3004/t_board"?null:<a className="btn btn-outline-light text-dark mx-1 mr-md-2 line" href="/t_board">T-board</a>}
-      <a className="btn btn-outline-light mr-md-2 line text-dark"  onClick={()=>deslogear()}>{ t("HeaderT.log_out")}</a>
+      <a className="btn btn-outline-light mr-md-2 line text-dark salir"  onClick={()=>deslogear()}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-out"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg> { t("HeaderT.log_out")}</a>
         
 
     </div>
--- src/components/T_board.jsx
+++ src/components/T_board.jsx
@@ -88,7 +88,7 @@ const T_board = () => {
                                  <li class="nav-item menu-items">
                                    <div class="nav-link" data-toggle="collapse">
                                      <span class="menu-icon">
-                                       <i class="mdi mdi-laptop"></i>
+                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>
                                        </span>
                                        <span class="menu-title">{t("HeaderT.projects")}</span>
                                        <i class="menu-arrow"></i>
@@ -96,21 +96,21 @@ const T_board = () => {
                                        </li>
                                        <li class="nav-item menu-items">
                                          <div class="nav-link" data-toggle="collapse">
-                                           <span class="menu-icon"><i class="mdi mdi-playlist-play"></i></span>
+                                           <span class="menu-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg></span>
                                            <span class="menu-title">{t("HeaderT.projectsM")}</span>
                                            <i class="menu-arrow"></i>
                                            </div>
                                           </li>
                                           <li class="nav-item menu-items">
                                             <div class="nav-link" data-toggle="collapse">
-                                              <span class="menu-icon"><i class="mdi mdi-table-large"></i></span>
+                                              <span class="menu-icon"><span class="fa fa-fw fa-wrench"></span></span>
                                               <span class="menu-title">{t("HeaderT.projectsS")}</span>
                                               <i class="menu-arrow"></i></div>
                                           </li></ul>
                                           <div className="nav-link">
-                <input type="text" name="text" placeholder={t("HeaderT.idPass")} className="form-control new" value={nameProject}  onKeyDown={(e) =>handleKeyDown(e)} onChange={e => { setNameProject(e.target.value) }}  />
-              </div>
-              <button className="btn btn-primary mt-2 mr9" onClick={() => {crearProject();console.log(name1)}}>{t("HeaderT.projectsNews")}</button>
+                                            <input type="text" name="text" placeholder={t("HeaderT.idPass")} className="form-control new" value={nameProject}  onKeyDown={(e) =>handleKeyDown(e)} onChange={e => { setNameProject(e.target.value) }}  />
+                                          </div>
+                                          <button className="btn btn-primary mt-2 mr9 plu" onClick={() => {crearProject();console.log(name1)}}> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg> {t("HeaderT.projectsNews")}</button>
                                           </nav>
                                             <div className="w9 altoCompleto vt">
                                               <div className="jumbotron p-2 mb-0">
