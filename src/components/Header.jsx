e7f99d6d1e6e97eaaab6d55c92d9461d3da9a7fc
Rebeca Paredes <rebecavparedes@gmail.com>
2021-06-10 21:12:30 -0400

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,9 +20,9 @@ const Header = () => {
     
       <nav className="my-2 my-md-0 mr-md-1">
         <a className="p-2 text-dark pr-3" href="/pricing">{t("Header.plans_pricing")}</a>
-        <a className="p-2 text-dark pr-3" href="">{t("Header.help")}</a>
+        <a className="p-2 text-dark pr-3 help" href=""><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-help-circle"><circle cx="12" cy="12" r="10"></circle><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path><line x1="12" y1="17" x2="12.01" y2="17"></line></svg>{t("Header.help")}</a>
       </nav>
-
+ 
       <ul class="navbar-nav idioma mr-md-1 pr-2">
         <li class="nav-item dropdown language-dropdown">
           {
